1adb54f755f5b95780f24387a897c1c6ca36db14
Yavor Mihaylov <yavor.mihaylov@trading212.com>
2016-09-10 17:41:15 +0300

    Add file for quickstart

---
+++ 2. Data structures, algorithms and complexity/quicksort.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// http://www.algolist.net/Algorithms/Sorting/Quicksort
+
+int main()
+{
+	int unsorted_array[9] = {1, 12, 5, 26, 7, 14, 3, 7, 2};
+
+	std::cout << unsorted_array;
+}
